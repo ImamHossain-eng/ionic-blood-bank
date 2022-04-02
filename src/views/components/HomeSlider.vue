cd70a0639ffe26f67e64bd91e75630ee0042fa22
ImamHossain-eng <babubpx2762@gmail.com>
2022-04-02 21:59:10 +0600

    Update Homepage

---
+++ src/views/components/HomeSlider.vue
@@ -0,0 +1,35 @@
+<template>
+  <ion-page>
+    <ion-content>
+      <swiper>
+        <swiper-slide>
+            <ion-img :src="'assets/images/slider/hub1.jpg'" />
+        </swiper-slide>
+        <swiper-slide>
+            <ion-img :src="'assets/images/slider/hub2.jpg'" />
+        </swiper-slide>
+        <swiper-slide>
+            <ion-img :src="'assets/images/slider/hub3.jpg'" />
+        </swiper-slide>
+      </swiper>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import { IonContent, IonPage, IonImg } from '@ionic/vue';
+
+import 'swiper/css';
+import '@ionic/vue/css/ionic-swiper.css';
+export default {
+    components: {
+        Swiper,
+        SwiperSlide,
+        IonContent,
+        IonPage,
+        IonImg,
+    }
+    
+}
+</script>
--- src/views/pages/HomePage.vue
+++ src/views/pages/HomePage.vue
@@ -9,8 +9,12 @@
             </ion-toolbar>
         </ion-header>
         <ion-content>
-            <h2 class="hub">Hamdard University Bangladesh</h2>
-           
+            <h4 class="hub">Hamdard University Bangladesh</h4>
+
+            <home-slider></home-slider>
+
+
+
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2737.3707078549696!2d90.61738044956421!3d23.598051100423035!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x37544d2890d5c195%3A0x9c93fd428ace86e9!2sHamdard%20University%20Bangladesh!5e1!3m2!1sen!2sbd!4v1648842135436!5m2!1sen!2sbd" width="600" height="500" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
            
         </ion-content>
@@ -18,9 +22,10 @@
 </template>
 <script>
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonMenuButton } from '@ionic/vue'
+import HomeSlider from '../components/HomeSlider.vue'
 export default {
     components: {
-        IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonMenuButton
+        IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonMenuButton, HomeSlider,
     }
     
 }
